b23185ae8721554e04bf689ae9663e6e3593f189
LeNinjaHD <cedric@leprofi.com>
2020-10-23 13:01:02 +0200

    Better Login Design

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("mysql.php");
+require("mysql.php");
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -17,6 +17,7 @@ include("mysql.php");
 
     <!-- Bootstrap-CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.5/examples/floating-labels/floating-labels.css" rel="stylesheet">
 
     <!-- Besondere Stile für diese Vorlage -->
     <style>
@@ -38,67 +39,6 @@ body {
   height: 60px;
   background-color: #f5f5f5;
 }
-
-
-/* Custom page CSS
--------------------------------------------------- */
-/* Not required for template or sticky footer method. */
-
-body > .container {
-  padding: 60px 15px 0;
-}
-.container .text-muted {
-  margin: 20px 0;
-}
-
-.footer > .container {
-  padding-right: 15px;
-  padding-left: 15px;
-}
-
-code {
-  font-size: 80%;
-}
-body {
-  /*padding-top: 40px;*/
-  padding-bottom: 40px;
-  background-color: #eee;
-}
-
-.form-signin {
-  max-width: 330px;
-  padding: 15px;
-  margin: 0 auto;
-}
-.form-signin .form-signin-heading,
-.form-signin .checkbox {
-  margin-bottom: 10px;
-}
-.form-signin .checkbox {
-  font-weight: normal;
-}
-.form-signin .form-control {
-  position: relative;
-  height: auto;
-  -webkit-box-sizing: border-box;
-     -moz-box-sizing: border-box;
-          box-sizing: border-box;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="text"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
 </style>
 
     <!-- Unterstützung für Media Queries und HTML5-Elemente in IE8 über HTML5 shim und Respond.js -->
@@ -110,40 +50,6 @@ body {
 
   <body>
 
-    <!-- Fixierte Navbar -->
-    <nav class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Navigation ein-/ausblenden</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">LeChat</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse ">
-          <ul class="nav navbar-nav">
-            <li><a href="index.php">Start</a></li>
-            <li><a href="chat.php">Chat</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mehr <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li class="dropdown-header">Schnellzugriffsleiste</li>
-                <li><a href="settings.php"><span class="glyphicon glyphicon-wrench"></span> Einstellungen</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Weiteres</li>
-                <li><a href="https://github.com/LeNinjaHD/LeChat">GitHub Repository von LeChat</a></li>
-                <li><a href="https://www.spigotmc.org/resources/73863/">SpigotMC Seite von LeChat</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Registrieren</a></li>
-            <li class="active"><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
   <?php
 if(isset($_GET['login'])) {
     $username = $_POST['username'];
@@ -156,7 +62,7 @@ if(isset($_GET['login'])) {
     //Überprüfung des Passworts
     if ($user !== false && password_verify($passwort, $user['passwort'])) {
         $_SESSION['userid'] = $user['id'];
-        die('Login erfolgreich. Weiter zum <a href="chat.php">Chat</a>');
+        die('<div class="container"><div class="alert alert-success" role="alert">Login erfolgreich. Weiter zum <a href="chat.php">Chat</a></div></div>');
     } else {
         $errorMessage = "E-Mail oder Passwort war ungültig<br>";
     }
@@ -168,16 +74,31 @@ if(isset($errorMessage)) {
     echo $errorMessage;
 }
 ?>
-<div class="container">
-<form class="form-signin" action="?login=1" method="post">
-<input type="text" size="40" maxlength="250" name="username" class="form-control" placeholder="Username">
-<input type="password" size="40"  maxlength="250" name="passwort" class="form-control" placeholder="Passwort"><br>
+<form class="form-signin" method="post" action="?login=1">
+  <div class="text-center mb-4">
+    <img class="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">LeChat</h1>
+    <p>You need an Account to use LeChat. If you don't have one, you can create one <a href="register.php">here</a></p>
+  </div>
 
-<button class="btn btn-lg btn-primary btn-block" type="submit">Absenden <span class="glyphicon glyphicon-send"></span></button>
+  <div class="form-label-group">
+    <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
+    <label for="username">Username</label>
+  </div>
+
+  <div class="form-label-group">
+    <input type="password" name="passwort" id="passwort" class="form-control" placeholder="passwort" required>
+    <label for="inputPassword">Password</label>
+  </div>
+
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Remember me
+    </label>
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2020</p>
 </form>
-</div>
-<p><center>
-Hast du noch keinen Account? <form action="register.php"><button type="submit" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-user"></span> Registrieren</button></form></center>
 <!-- Bootstrap-JavaScript
 ================================================== -->
 <!-- Am Ende des Dokuments platziert, damit Seiten schneller laden -->
